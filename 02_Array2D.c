faaab0eb8c70d90894c9a3e499be689cdb5fd05e
NEWJYH <NEWJYH>
2022-07-17 17:50:32 +0900

    Array

--- 02_Array2D.c
+++ 02_Array2D.c
@@ -16,6 +16,7 @@ int main() {
 	
 	// 안쪽 배열 크기의 의미는 몇칸 이동하면 다음 배열인지를 의미한다.
 
+	/*
 	printf("2차원 배열 \"class\"의 크기 : %d\n", sizeof(class));
 	printf("2차원 배열 \"class[0]\"의 크기 : %d\n\n", sizeof(class[0]));
 							
@@ -27,6 +28,43 @@ int main() {
 		}
 		printf("\n");
 	}
+
+	for (int y = 0; y < sizeof(class) / sizeof(class[y]); y++) {
+		printf("%d", class[y][2]);
+	}
+	printf("\n");
+	for (int x = 0; x < sizeof(class[1]) / sizeof(int); x++) {
+		printf("%d", class[1][x]);
+	}
+	*/
+
+	// 빙고
+	// 가로줄의 빙고가 몇개인지 확인
+	
+	char bingo[3][3] =
+	{
+		{0, 1, 1},
+		{0, 1, 0},
+		{1, 1, 1}
+	};
+	
+	int count = 0;
+
+	for (int i = 0; i < 3; i++) {
+		int j = 0;
+		if (bingo[i][j] == 1) {
+			j++;
+			if (bingo[i][j] == 1) {
+				j++;
+				if (bingo[i][j] == 1) {
+					j++;
+					count++;
+				}
+			}
+		}
+	}
+
+	printf("가로줄 빙고의 갯수는 : %d", count);
 
 	// 이중 For 문
 	/*
