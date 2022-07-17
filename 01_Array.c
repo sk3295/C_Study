ed281a599d97e683b8e9aa041cbd46624162d895
NEWJYH <NEWJYH>
2022-07-17 15:49:20 +0900

    Array

---
+++ 01_Array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+	// 이니셜라이저 : 초기화
+	char gameName[3] = {'E', 'X', 0};
+	char test[] = "EX";
+	char largeArray[1000] = { 'a' };
+
+	gameName[1] = 'A';
+	gameName[0] = gameName[1];
+	
+	for (int i = 0; i < 1000; i++) {
+		largeArray[i] = 'a';
+	}
+
+	for (int i = 0; i < 1000; i++) {
+		char a = 'a';
+		largeArray[i] = a;
+		a++;
+	}
+
+	largeArray[999] = 0;
+
+	printf(gameName);
+	printf(test);
+	printf(largeArray);
+	printf("\n여기서 출력하는 문자열도 배열");
+
+
+
+}
