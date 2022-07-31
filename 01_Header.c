1163c9c4456ce00a7bb4a4d41ed78b7bc107a258
NEWJYH <NEWJYH>
2022-07-31 15:15:22 +0900

    Header

---
+++ 01_Header.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "TestHeader.h"
+
+#define 메인 int main()
+#define 출력 printf
+#define 정수 int
+#define 실수 float
+#define 나의이름출력 printf("윤현기")
+#define 엔터치기 ('\n')
+
+메인{
+	정수 더한횟수 = 0;
+	출력("더한 횟수 : %d", 더한횟수);
+	엔터치기;
+
+	나의이름출력;
+	엔터치기;
+	엔터치기;
+	엔터치기;
+	나의이름출력;
+}
